1be90bd970fc902fa28911ead7725e01a631dd41
asifikbal01890 <asifikbal01890@gmail.com>
2024-07-07 17:50:17 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,25 @@
+// 5^4
+// console.log(Math.pow(5, 4));
+// console.log(Math.abs(Math.random()*6));
+
+// const raju = 44;
+// const sajib = 88;
+// console.log(Math.abs(raju - sajib));
+
+// if (Math.abs(raju - sajib) < 5) {
+//     console.log('you are can be friend');
+// } else {
+//     console.log('dure thako');
+// }
+
+const num = 2.99999999;
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+
+
+// concept of ludo
+
+console.log(Math.ceil(Math.random()*6));
+
+
---
+++ string.js
@@ -0,0 +1,47 @@
+// const lyrics = 'tumi bonshu kala PaKhi. ami jno ki. bosonto kale tomai bolte pari ni. sada sada kala kala. rong jomece sada kala.';
+/* const lyricsLowerCase = lyrics.toLowerCase();
+const searchKeyword = 'PAKHI';
+const searchKeywordlowerCase = searchKeyword.toLowerCase(); */
+// console.log(searchKeywordlowerCase);
+
+
+// const result = lyricsLowerCase.includes(searchKeywordlowerCase);
+// console.log(result);
+
+
+// const asif = 'I am a web developer'
+// console.log(asif.toLocaleLowerCase());
+// console.log(asif.toUpperCase());
+
+
+const lyrics = 'tumi bondhu kala PaKhi. ami jno ki. bosonto kale tomai bolte pari ni. sada sada kala kala. rong jomece sada kala';
+
+// const newLyrics = lyrics.slice(4, 11);
+
+let lyricsArray = lyrics.split(' ');
+
+// console.log(lyrics.replace(' bondhu', ''));
+
+
+
+// console.log(lyrics.split('.'));
+
+
+// console.log(lyrics.indexOf(' '));
+// console.log(lyrics[2]);
+
+const asif = '                                 I am a web developer ';
+// console.log(asif.trim());
+// console.log(asif.endsWith('eveloper'));
+
+const a = [
+    'tumi',   'bonshu',  'kala',
+    'PaKhi.', 'ami',     'jno',
+    'ki.',    'bosonto', 'kale',
+    'tomai',  'bolte',   'pari',
+    'ni.',    'sada',    'sada',
+    'kala',   'kala.',   'rong',
+    'jomece', 'sada',    'kala.'
+  ]
+
+//   console.log(a.join(' '));
